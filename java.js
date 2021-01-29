8413cc0a29ed801008cfd2d4da8e6e5fd46ad8f6
Jim Nguyen <jimnguyen220@gmail.com>
2021-01-28 19:46:37 -0600

    declared variables for elements on restaurants page

---
+++ java.js
@@ -0,0 +1,13 @@
+var resultOne = $('#result-1-1');
+var resultTwo = $('#result-1-2');
+var resultThree = $('#result-1-3');
+var resultFour = $('#result-2-1');
+var resultFive = $('#result-2-2');
+var resultSix = $('#result-2-3');
+var resultSeven = $('#result-3-1');
+var resultEight = $('#result-3-2');
+var resultNine = $('#result-3-3');
+var resultTen = $('#result-4-1');
+var resultEleven = $('#result-4-2');
+var resultTwelve = $('#result-4-3');
+
