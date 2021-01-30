70f1e48dc2ced0a37333e0dfbf5fc79ff30aba20
LittleMagician <bthaoxaochay@gmail.com>
2021-01-30 13:47:48 -0600

    for loop for parsing through object and appending

--- java.js
+++ java.js
@@ -21,18 +21,17 @@ $('#submit-btn').on('click', function(event){
     }
     localStorage.setItem("userInput", JSON.stringify(userInput));
     
-    // window.open("restaurants.html");
+    window.open("restaurants.html");
 
 
-    // Zip code entered and submit button pressed
-        // Loop through all zip codes and get the restaurants in that zip code
-    var citystateVal = $(this).siblings(".zipCodeForm").val();
+    // Ajax call to get restaurants in city
+    var citystateVal = $("#city-state").val();
     console.log(citystateVal);
 
     $.ajax({
         url: "https://developers.zomato.com/api/v2.1/cities?q=" + citystateVal,
         headers: { 'user-key': '7f4fa469b70c80542b1210267c2e78aa' }
-    }).then(function (cities) {
+    }).done(function (cities) {
 
             var citiesID = cities.location_suggestions[0].id;
             console.log(citiesID);
@@ -43,7 +42,30 @@ $('#submit-btn').on('click', function(event){
             headers: { 'user-key': '59d32d7639d297f576ffc1c3d64a97f4' }
         }).then(function (search) {
             console.log(search);
+
+            for (var i = 0; i < search.restaurants.length; i++) {
+
+                if (i>3){
+                    break;
+                }
+
+                // Restaurant Image
+                // Restaurant Name
+                var restaurantName = search.restaurants[i].restaurant.name;
+                // Restaurant Rating
+                // Restaurant Description
+                // Restaurant Home Page
+                var restaurantURL = search.restaurants[i].restaurant.url;
+                // Restaurant Directions            
+                var restaurantDirections = search.restaurants[i].restaurant.location.address;
+
+                console.log(restaurantName);
+                console.log(restaurantURL);
+                console.log(restaurantDirections);
+            };
             
+
+
         }) // Search Ajax Call
 
     }); // Cities Ajax Call
