3102101b60d08b2ac8cf540d3762b4f406d16658
LittleMagician <bthaoxaochay@gmail.com>
2021-01-31 13:15:50 -0600

    java.js - restaurant name appends to card, but to every card

--- java.js
+++ java.js
@@ -16,9 +16,9 @@ var resultTwelve = $('#result-4-3');
 //opens restaurants page - rename element on index.html for submit-btn
 $('#submit-btn').on('click', function(event){
     event.preventDefault();
-    var userInput = {
-        zipcodeInput: $('#zipcode-input').val(),
-    }
+    
+    // Local storing city and state
+    var userInput = $("#city-state").val();
     localStorage.setItem("userInput", JSON.stringify(userInput));
     //updated this to open page in same window instead of a different tab
     window.location.href="restaurants.html";
@@ -73,3 +73,54 @@ $('#submit-btn').on('click', function(event){
    
 
 }); // Click Function
+
+// Onload Function
+function loadRestaurantInfo() {
+
+    // Ajax call to get restaurants in city
+    var citystateVal = localStorage.getItem("userInput");
+    console.log(citystateVal);
+
+    $.ajax({
+        url: "https://developers.zomato.com/api/v2.1/cities?q=" + citystateVal,
+        headers: { 'user-key': '7f4fa469b70c80542b1210267c2e78aa' }
+    }).done(function (cities) {
+
+        var citiesID = cities.location_suggestions[0].id;
+        console.log(citiesID);
+        
+
+        $.ajax({
+            url: "https://developers.zomato.com/api/v2.1/search?entity_id=" + citiesID + "&entity_type=city",
+            headers: { 'user-key': '59d32d7639d297f576ffc1c3d64a97f4' }
+        }).then(function (search) {
+            console.log(search);
+
+            for (var i = 0; i < search.restaurants.length; i++) {
+
+                if (i>3){
+                    break;
+                }
+
+                // Restaurant Image
+                // Restaurant Name
+                var restaurantName = search.restaurants[i].restaurant.name;
+                // Restaurant Rating
+                // Restaurant Description
+                // Restaurant Home Page
+                var restaurantURL = search.restaurants[i].restaurant.url;
+                // Restaurant Directions            
+                var restaurantDirections = search.restaurants[i].restaurant.location.address;
+
+                console.log(restaurantName);
+                console.log(restaurantURL);
+                console.log(restaurantDirections);
+
+                $(".card-title").append(restaurantName);
+            }
+            
+
+
+        }) // Search Ajax Call
+    }); // Cities Ajax Call
+} // Onload Function
