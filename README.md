36d852d8df37c7542319c09a2c341bb0938b71e5
Jim Nguyen <jimnguyen220@gmail.com>
2021-01-31 20:50:33 -0600

    pushing up changes to maps page

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # projectOne
+
+
+
+Resources:
+Google Maps JavaScript API Tutorial
+https://www.youtube.com/watch?v=Zxf1mnP5zcw
+
+
+Google Maps Tutorial:
+Geolocation
+https://www.youtube.com/watch?v=oGMs3sP6Z3c
+Maps
+https://youtu.be/cB6FccF0j14
+Directions
+https://www.youtube.com/watch?v=W0juXNFLd6w
+
+https://developers.google.com/maps/documentation/javascript/overview
--- maps.js
+++ maps.js
@@ -1,50 +1,64 @@
-// npm install @googlemaps/js-api-loader
-// import { Loader } from "@googlemaps/js-api-loader"
-// const loader = new Loader({
-//     apiKey: "AIzaSyBqaNR3uvzsucoC2tlWSAao5fSmurSGR1g",
-//     version: "weekly",
-//     ...additionalOptions,
-//   });
-//   loader.load().then(() => {
-//     map = new google.maps.Map(document.getElementById("map"), {
-//       center: { lat: -34.397, lng: 150.644 },
-//       zoom: 8,
-//     });
-//   });
-
-
-let map;
-
 function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 44.977, lng: -93.264 },
+  
+  var location = { lat: 44.977, lng: -93.264 };
+  var map = new google.maps.Map(document.getElementById("map"), {
+    center: location,
     zoom: 13,
   });
+  var marker = new google.maps.Marker ({
+    position: location,
+    map: map
+  })
 }
 
-var x = document.getElementById("demo");
-
-
-//to get this to work, need to comment out lines 13 - 16 in maps.html
 function getLocation() {
-  if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(showPosition);
-  } else { 
-    x.innerHTML = "Geolocation is not supported by this browser.";
-  }
 }
+
+var coordinates = {};
 
 function showPosition(position) {
   console.log("Latitude: " + position.coords.latitude);
   console.log("Longitude: " + position.coords.longitude);
 
-  var coordinates = {
-    latitude: position.coords.latitude,
-    longitude: position.coords.longitude
+  mapLat =  position.coords.latitude;
+  mapLng =  position.coords.longitude
+
+  coordinates = {
+    lat: position.coords.latitude,
+    lng: position.coords.longitude
   }
 
-  localStorage.setItem("coordinates", JSON.stringify(coordinates));
+  getMap();
 
+  // localStorage.setItem("coordinates", JSON.stringify(coordinates));
+ 
+  // getDirections();
 }
 
+function getMap() {
+  var mapOptions = {
+    zoom: 14,
+    center: new google.maps.Map(mapLat, mapLng)
+  }
+  map = new google.maps.Map(document.getElementById("map"), mapOptions);
+}
+
+// function getDirections(origin, destination) {
+//   var directionsService = new google.maps.DirectionsService();
+//   var direcstionsDisplay = new google.maps.DirectionsRender();
+//   request = {
+//     origin: origin,
+//     destination: destination,
+//     travelMode: 'DRIVING'
+//   }
+//   direcstionsDisplay.setMap(map);
+//   directionsService.route(request, (result, status) {
+//     if (status == "ok") {
+//       directionsDisplay.setDirections(result);
+//     }
+//   })
+// }
+
 $('#get-location').on('click', getLocation);
+
