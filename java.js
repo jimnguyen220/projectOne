3160a9c36f12b1e264c3404b304505f369ff8f44
LittleMagician <bthaoxaochay@gmail.com>
2021-02-01 18:56:31 -0600

    removed ajax call in submit button function

--- java.js
+++ java.js
@@ -23,55 +23,6 @@ $('#submit-btn').on('click', function(event){
     //updated this to open page in same window instead of a different tab
     window.location.href="restaurants.html";
 
-
-
-    // Ajax call to get restaurants in city
-    var citystateVal = $("#city-state").val();
-    console.log(citystateVal);
-
-    $.ajax({
-        url: "https://developers.zomato.com/api/v2.1/cities?q=" + citystateVal,
-        headers: { 'user-key': '7f4fa469b70c80542b1210267c2e78aa' }
-    }).done(function (cities) {
-
-            var citiesID = cities.location_suggestions[0].id;
-            console.log(citiesID);
-        
-
-        $.ajax({
-            url: "https://developers.zomato.com/api/v2.1/search?entity_id=" + citiesID + "&entity_type=city",
-            headers: { 'user-key': '59d32d7639d297f576ffc1c3d64a97f4' }
-        }).then(function (search) {
-            console.log(search);
-
-            for (var i = 0; i < search.restaurants.length; i++) {
-
-                if (i>3){
-                    break;
-                }
-
-                // Restaurant Image
-                // Restaurant Name
-                var restaurantName = search.restaurants[i].restaurant.name;
-                // Restaurant Rating
-                // Restaurant Description
-                // Restaurant Home Page
-                var restaurantURL = search.restaurants[i].restaurant.url;
-                // Restaurant Directions            
-                var restaurantDirections = search.restaurants[i].restaurant.location.address;
-
-                console.log(restaurantName);
-                console.log(restaurantURL);
-                console.log(restaurantDirections);
-            }
-            
-
-
-        }) // Search Ajax Call
-    }); // Cities Ajax Call
-
-   
-
 }); // Click Function
 
 // Onload Function
